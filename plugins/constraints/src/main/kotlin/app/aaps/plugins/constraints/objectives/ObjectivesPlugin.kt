9a49b224615823db5c5e988166269dc485c82bfa
Marcin Markowski <marcin@marklodev.com>
2025-05-23 23:39:08 +0200

    Setting all objectives as finished

--- plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/objectives/ObjectivesPlugin.kt
+++ plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/objectives/ObjectivesPlugin.kt
@@ -139,6 +139,6 @@ class ObjectivesPlugin @Inject constructor(
         return value
     }
 
-    override fun isAccomplished(index: Int) = objectives[index].isAccomplished
-    override fun isStarted(index: Int): Boolean = objectives[index].isStarted
+    override fun isAccomplished(index: Int) = true
+    override fun isStarted(index: Int): Boolean = true
 }
